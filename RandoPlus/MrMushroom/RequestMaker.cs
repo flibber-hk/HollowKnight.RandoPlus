106e639e111f4427dca30ea9533250fee0d76812
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-08 15:55:49 +0000

    Improvements, and add deranged constraint

--- RandoPlus/MrMushroom/RequestMaker.cs
+++ RandoPlus/MrMushroom/RequestMaker.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using ItemChanger;
+using RandomizerCore;
+using RandomizerCore.Randomization;
 using RandomizerMod.RandomizerData;
 using RandomizerMod.RC;
 
@@ -9,21 +11,75 @@ namespace RandoPlus.MrMushroom
 {
     public static class RequestMaker
     {
+        static readonly HashSet<string> mushrooms = new()
+        {
+            Consts.MrMushroomFungalWastes,
+            Consts.MrMushroomKingdomsEdge,
+            Consts.MrMushroomDeepnest,
+            Consts.MrMushroomHowlingCliffs,
+            Consts.MrMushroomAncientBasin,
+            Consts.MrMushroomFogCanyon,
+            Consts.MrMushroomKingsPass,
+        };
+
         public static void Hook()
         {
             RequestBuilder.OnUpdate.Subscribe(50, AddMrMushroom);
+            RequestBuilder.OnUpdate.Subscribe(101, DerangeMrMushroom);
+            RequestBuilder.OnUpdate.Subscribe(-499, MatchMrMushroom);
+        }
+
+        private static void DerangeMrMushroom(RequestBuilder rb)
+        {
+            if (!rb.gs.CursedSettings.Deranged) return;
+            if (!RandoPlus.GS.MrMushroom) return;
+
+            static bool NotVanillaMushroom(IRandoItem item, IRandoLocation location)
+            {
+                if (item.Name == Consts.MrMushroomLevelUp && mushrooms.Contains(location.Name))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            foreach (ItemGroupBuilder gb in rb.EnumerateItemGroups())
+            {
+                if (gb.strategy is DefaultGroupPlacementStrategy dgps)
+                {
+                    dgps.Constraints += NotVanillaMushroom;
+                }
+            }
+        }
+
+        private static void MatchMrMushroom(RequestBuilder rb)
+        {
+            if (!RandoPlus.GS.MrMushroom) return;
+
+            rb.OnGetGroupFor.Subscribe(-100f, MatchMushroomGroup);
+
+            bool MatchMushroomGroup(RequestBuilder rb, string item, RequestBuilder.ElementType type, out GroupBuilder gb)
+            {
+                if (item == Consts.MrMushroomLevelUp && (type == RequestBuilder.ElementType.Unknown || type == RequestBuilder.ElementType.Item))
+                {
+                    gb = rb.GetGroupFor(ItemNames.Lore_Tablet_City_Entrance);
+                    return true;
+                }
+                else if (mushrooms.Contains(item) && (type == RequestBuilder.ElementType.Unknown || type == RequestBuilder.ElementType.Location))
+                {
+                    gb = rb.GetGroupFor(ItemNames.Lore_Tablet_City_Entrance);
+                    return true;
+                }
+                gb = default;
+                return false;
+            }
         }
 
         private static void AddMrMushroom(RequestBuilder rb)
         {
             if (!RandoPlus.GS.MrMushroom) return;
 
-            ItemGroupBuilder mushGroup = rb.GetItemGroupFor(ItemNames.Lore_Tablet_City_Entrance);
-
-            for (int i = 0; i < 7; i++)
-            {
-                mushGroup.Items.Add(Consts.MrMushroomLevelUp);
-            }
+            rb.AddItemByName(Consts.MrMushroomLevelUp, 7);
             rb.EditItemRequest(Consts.MrMushroomLevelUp, info =>
             {
                 info.getItemDef = () => new()
@@ -35,20 +91,9 @@ namespace RandoPlus.MrMushroom
                 };
             });
 
-            HashSet<string> mushrooms = new()
-            {
-                Consts.MrMushroomFungalWastes,
-                Consts.MrMushroomKingdomsEdge,
-                Consts.MrMushroomDeepnest,
-                Consts.MrMushroomHowlingCliffs,
-                Consts.MrMushroomAncientBasin,
-                Consts.MrMushroomFogCanyon,
-                Consts.MrMushroomKingsPass,
-            };
-
             foreach (string loc in mushrooms)
             {
-                mushGroup.Locations.Add(loc);
+                rb.AddLocationByName(loc);
                 rb.EditLocationRequest(loc, info =>
                 {
                     info.getLocationDef = () => new()
@@ -59,24 +104,6 @@ namespace RandoPlus.MrMushroom
                         AdditionalProgressionPenalty = false,
                     };
                 });
-            }
-
-            rb.OnGetGroupFor.Subscribe(-100f, MatchMushroomGroup);
-
-            bool MatchMushroomGroup(RequestBuilder rb, string item, RequestBuilder.ElementType type, out GroupBuilder gb)
-            {
-                if (item == Consts.MrMushroomLevelUp && (type == RequestBuilder.ElementType.Unknown || type == RequestBuilder.ElementType.Item))
-                {
-                    gb = mushGroup;
-                    return true;
-                }
-                else if (mushrooms.Contains(item) && (type == RequestBuilder.ElementType.Unknown || type == RequestBuilder.ElementType.Location))
-                {
-                    gb = mushGroup;
-                    return true;
-                }
-                gb = default;
-                return false;
             }
         }
     }
