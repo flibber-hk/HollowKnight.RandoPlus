4a8523d768cb12738b6a1d4fdd247c2d8883df6e
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-10 17:51:18 +0000

    Correct group matcher for removeusefulitems

--- RandoPlus/RemoveUsefulItems/RequestModifier.cs
+++ RandoPlus/RemoveUsefulItems/RequestModifier.cs
@@ -9,32 +9,40 @@ namespace RandoPlus.RemoveUsefulItems
 {
     public static class RequestModifier
     {
+        public static readonly (string oldItem, string newItem, string errorMessage, string skipType, Func<bool> isActive)[] settings = new (string oldItem, string newItem, string errorMessage, string skipType, Func<bool> isActive)[]
+        {
+            (ItemNames.Lumafly_Lantern, Consts.NoLantern,
+                "Dark room skips required for no Lantern", nameof(RandomizerMod.Settings.SkipSettings.DarkRooms),
+                () => RandoPlus.GS.NoLantern),
+            (ItemNames.Ismas_Tear, Consts.NoTear,
+                "Acid skips required for no Tear", nameof(RandomizerMod.Settings.SkipSettings.AcidSkips),
+                () => RandoPlus.GS.NoTear),
+            (ItemNames.Swim, Consts.NoSwim,
+                "Acid skips required for no Swim", nameof(RandomizerMod.Settings.SkipSettings.AcidSkips),
+                () => RandoPlus.GS.NoSwim)
+        };
+
         public static void Hook()
         {
             RequestBuilder.OnUpdate.Subscribe(-499f, SetupRefs);
 
-            RequestBuilder.OnUpdate.Subscribe(50f, CreateRemover(ItemNames.Lumafly_Lantern, Consts.NoLantern, 
-                "Dark room skips required for no Lantern", nameof(RandomizerMod.Settings.SkipSettings.DarkRooms), 
-                () => RandoPlus.GS.NoLantern));
-            RequestBuilder.OnUpdate.Subscribe(50f, CreateRemover(ItemNames.Ismas_Tear, Consts.NoTear,
-                "Acid skips required for no Tear", nameof(RandomizerMod.Settings.SkipSettings.AcidSkips),
-                () => RandoPlus.GS.NoTear));
-            RequestBuilder.OnUpdate.Subscribe(50f, CreateRemover(ItemNames.Swim, Consts.NoSwim,
-                "Acid skips required for no Swim", nameof(RandomizerMod.Settings.SkipSettings.AcidSkips),
-                () => RandoPlus.GS.NoSwim));
+            foreach (var tuple in settings)
+            {
+                RequestBuilder.OnUpdate.Subscribe(50f, CreateRemover(tuple.oldItem, tuple.newItem, tuple.errorMessage, tuple.skipType, tuple.isActive));
+            }
         }
 
         private static void SetupRefs(RequestBuilder rb)
         {
             if (!RandoPlus.GS.Any) return;
 
-            foreach (string newItem in new[] { Consts.NoTear, Consts.NoLantern, Consts.NoSwim })
+            foreach (var tuple in settings)
             {
-                rb.EditItemRequest(newItem, info =>
+                rb.EditItemRequest(tuple.newItem, info =>
                 {
                     info.getItemDef = () => new ItemDef()
                     {
-                        Name = newItem,
+                        Name = tuple.newItem,
                         Pool = Consts.RemoveUsefulItems,
                         MajorItem = false,
                         PriceCap = 500
@@ -45,9 +53,9 @@ namespace RandoPlus.RemoveUsefulItems
 
                 bool MatchGroup(RequestBuilder rb, string item, RequestBuilder.ElementType type, out GroupBuilder gb)
                 {
-                    if (item == newItem && (type == RequestBuilder.ElementType.Item || type == RequestBuilder.ElementType.Unknown))
+                    if (item == tuple.newItem && (type == RequestBuilder.ElementType.Item || type == RequestBuilder.ElementType.Unknown))
                     {
-                        gb = rb.GetGroupFor(item, type);
+                        gb = rb.GetGroupFor(tuple.oldItem, type);
                         return true;
                     }
                     gb = default;
