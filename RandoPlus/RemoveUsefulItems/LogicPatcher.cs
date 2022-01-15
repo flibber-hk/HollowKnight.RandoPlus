fc6983d7f282beeeda813ff60dd8feadd907fb2e
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2022-01-15 17:41:52 +0000

    Use RC methods for logic substitutions

--- RandoPlus/RemoveUsefulItems/LogicPatcher.cs
+++ RandoPlus/RemoveUsefulItems/LogicPatcher.cs
@@ -24,29 +24,22 @@ namespace RandoPlus.RemoveUsefulItems
         {
             if (!RandoPlus.GS.Any) return;
 
-            void ProvideLogicSubstitution(string name, string pre, string post)
-            {
-                LogicClauseBuilder lcb = new(lmb.LogicLookup[name]);
-                lcb.Subst(lmb.LP.GetTermToken(pre), new LogicClause(post, lmb.LP));
-                lmb.LogicLookup[name] = new(lcb);
-            }
-
             // Lantern
-            ProvideLogicSubstitution("Mines_33[right1]", "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
-            ProvideLogicSubstitution("Mines_33[left1]", "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
-            ProvideLogicSubstitution("Defeated_No_Eyes", "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
-            ProvideLogicSubstitution(LocationNames.Boss_Essence_No_Eyes, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
-            ProvideLogicSubstitution(LocationNames.Geo_Rock_Crystal_Peak_Entrance, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
-            ProvideLogicSubstitution(LocationNames.Geo_Rock_Crystal_Peak_Entrance_Dupe_1, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
-            ProvideLogicSubstitution(LocationNames.Geo_Rock_Crystal_Peak_Entrance_Dupe_2, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS");
+            lmb.DoSubst(new("Mines_33[right1]", "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
+            lmb.DoSubst(new("Mines_33[left1]", "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
+            lmb.DoSubst(new("Defeated_No_Eyes", "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
+            lmb.DoSubst(new(LocationNames.Boss_Essence_No_Eyes, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
+            lmb.DoSubst(new(LocationNames.Geo_Rock_Crystal_Peak_Entrance, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
+            lmb.DoSubst(new(LocationNames.Geo_Rock_Crystal_Peak_Entrance_Dupe_1, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
+            lmb.DoSubst(new(LocationNames.Geo_Rock_Crystal_Peak_Entrance_Dupe_2, "LANTERN", "LANTERN | NOLANTERN + DARKROOMS"));
 
             // Isma's
-            ProvideLogicSubstitution("Fungus2_06[left2]", "ACID", "ACID | NOACID + WINGS + (LEFTDASH | (RIGHTCLAW + LEFTSUPERDASH))");
-            ProvideLogicSubstitution("Fungus2_06", "ACID", "ACID | NOACID + RIGHTDASH + (WINGS | RIGHTCLAW)");
-            ProvideLogicSubstitution("Fungus1_26[left1]", "ACID", "ACID | NOACID + LEFTSUPERDASH + LEFTDASH + WINGS");
-            ProvideLogicSubstitution("Deepnest_East_04", "ACID", "ACID | NOACID + RIGHTSKIPACID");
-            ProvideLogicSubstitution("Deepnest_East_04[left1]", "ACID", "ACID | NOACID + LEFTDASH + LEFTSUPERDASH + WINGS");
-            ProvideLogicSubstitution(LocationNames.Mask_Shard_Grey_Mourner, "ACID", "ACID | NOACID");
+            lmb.DoSubst(new("Fungus2_06[left2]", "ACID", "ACID | NOACID + WINGS + (LEFTDASH | (RIGHTCLAW + LEFTSUPERDASH))"));
+            lmb.DoSubst(new("Fungus2_06", "ACID", "ACID | NOACID + RIGHTDASH + (WINGS | RIGHTCLAW)"));
+            lmb.DoSubst(new("Fungus1_26[left1]", "ACID", "ACID | NOACID + LEFTSUPERDASH + LEFTDASH + WINGS"));
+            lmb.DoSubst(new("Deepnest_East_04", "ACID", "ACID | NOACID + RIGHTSKIPACID"));
+            lmb.DoSubst(new("Deepnest_East_04[left1]", "ACID", "ACID | NOACID + LEFTDASH + LEFTSUPERDASH + WINGS"));
+            lmb.DoSubst(new(LocationNames.Mask_Shard_Grey_Mourner, "ACID", "ACID | NOACID"));
 
             // Swim
             // No changes required
